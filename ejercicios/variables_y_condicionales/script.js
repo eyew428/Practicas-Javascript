55e05f54698ba0ba4126b19bf589bb4d4b9b698c
eyew428 <eyew428@gmail.com>
2026-04-15 21:59:33 -0300

    Modificación

--- ejercicios/variables_y_condicionales/script.js
+++ ejercicios/variables_y_condicionales/script.js
@@ -5,3 +5,60 @@
 >     console.log("Hola desconocido")
 >
 }
+
+
+let (edad =  18)
+if (edad >= 18) {
+    console.log("Eres mayor de edad")
+} else {
+    console.log("Eres menor de edad")
+}
+
+let clasificacion = 60
+
+if (clasificacion == 50) {
+    console.log("Aprobado")
+} else if (clasificacion <60) {
+    console.log("Reprobado")
+}
+
+switch (día) {
+    case 1:
+        console.log
+        ("Lunes");
+
+         case 2:
+        console.log
+        ("Martes");
+
+         case 3:
+        console.log
+        ("Miércoles");
+
+         case 4:
+        console.log
+        ("Jueves");
+
+         case 5:
+        console.log
+        ("Viernes");
+}
+
+if temp = 30;
+
+if (temp < 10) {
+    console.log("Hace frío")
+} else if (temp >= 10 && temp <= 25) {
+    console.log("templado");
+    else if (temp > 25) {
+        console.log("Hace calor")
+    }
+}
+
+let num1 = 5;
+
+if (num1 > 0) {
+    console.log("psitivo");
+} else if (num1 < 0) {
+    console.log("negativo");
+} else console.log("cero");
