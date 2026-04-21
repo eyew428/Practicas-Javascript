bfa493b430b692dbeba8f0604c5f5618b41943e0
eye428 <eyew428@gmail.com>
2026-04-20 23:05:09 -0300

    Cambios de JS

--- ejercicios/bucles_y_arrays/script.js
+++ ejercicios/bucles_y_arrays/script.js
@@ -20,14 +20,37 @@ while (contador >= 1) {
 
 const numeros = [4, 17, 3, 25, 8, 11];
 let mayor = numeros[0];
-for (let i = 0; i <= numeros.length-1; i++) {
-    if (numeros[i] > mayor) {
-        mayor = numeros[i];
+for (let contador = 0; contador < numeros.length; contador++) {
+    if (numeros[contador] > mayor) {
+        mayor = numeros[contador];
     }
+    console.log(numeros.length);
+    
 }
+
 console.log("El número mayor es: ", mayor);
 
 
 //Ejercicio 4
 
 
+const palabras = ["sol", "montaña", "mar", "elefante", "río", "cascada"];
+const palabrasLargas = [];
+for (let i = 0; i < palabras.length; i++) {
+    if (palabras[i].length > 4) {
+        palabrasLargas.push(palabras[i]);
+    }
+}
+console.log("Palabras largas: ", palabrasLargas);
+
+
+//Ejercicio 5
+
+const numero = 7;
+let multiplicador = 1;
+
+while (multiplicador <= 10) {
+    let resultado = numero * multiplicador;
+    console.log(`${numero} x ${multiplicador} = ${resultado}`);
+    multiplicador++;
+}
